3e6612c4eaa41e2b44bef8036f078a924d681bf9
jose blois <joseblois3@gmail.com>
2020-04-28 01:58:20 -0300

    Finished with the camera movement and locking it to the map

---
+++ app.js
@@ -0,0 +1,290 @@
+(function(window,undefined){
+var canvas=null,ctx=null;
+var player = null;
+var pressing = [], lastPress=null;
+var KEY_LEFT=37, KEY_UP=38,KEY_RIGHT=39,KEY_DOWN=40,KEY_ENTER=13;
+var pause=false,gameover=false;
+var cam = null;
+var worldWidth=0,worldHeight=0;
+var wall = [],
+lava = [],
+maps=[];
+maps[0] = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 2, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 1, 0, 0, 1],
+    [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+];
+function setMap (map,blocksize){
+    var col=0,row=0,columns=0,rows=0;
+    wall.length=0;
+    lava.length=0;
+    for (row = 0 ,rows=map.length; row<rows;row++){
+        for (col = 0,columns=map[row].length;col<columns;col++){
+            if (map[row][col]===1){
+                wall.push(new Rectangle2D(col*blocksize,row*blocksize,blocksize,blocksize,true));
+            }else if (map[row][col]===2){
+                lava.push(new Rectangle2D(col*blocksize,row*blocksize,blocksize,blocksize,true));
+            } 
+        }
+    }
+    worldWidth=columns * blocksize
+    worldHeight=rows * blocksize
+}
+function Rectangle2D (x,y,width,height,createFromTopLeft){
+    this.width = (width===undefined)?0:width;
+    this.height = (height===undefined)?this.width:height;
+        if(createFromTopLeft){
+            this.left = (x===undefined)?0:x;
+            // this.right = this.left + this.width;
+            this.top = (y===undefined)?0:y;
+            // this.bottom = this.top + this.height;
+        }else{
+            this.x =(x===undefined)?0:x;
+            this.y = (y===undefined)?0:y;
+        }
+}
+Rectangle2D.prototype = {
+    left : 0,
+    top : 0,
+    width:0,
+    height:0,
+    rotation:0,
+    rotationTransition:0,
+
+    get x() {
+        return this.left + this.width/2;
+    },
+    set x(value){
+        this.left = value - this.width/2;
+    },
+    get y (){
+        return this.top + this.height/2;
+    },
+    set y(value){
+        this.top = value - this.height/2;
+    },
+    get right(){
+        return this.left + this.width;
+    },
+    set right (value){
+        this.left = value - this.width;
+    },
+    get bottom(){
+        return this.top + this.height;
+    },
+    set bottom(value){
+        this.top = value -this.height;
+    },
+
+    contains: function(rect){
+        if(rect!==undefined){
+          return (this.left < (rect.left || rect.x)&&
+                this.right > (rect.right || rect.x)&&
+                this.top < (rect.top || rect.y)&&
+                this.bottom > (rect.bottom || rect.y));
+        }
+    },
+    intersects : function(rect){
+        if (rect!==undefined){
+            return(this.left < rect.right &&
+                this.right > rect.left &&
+                this.bottom > rect.top &&
+                this.top < rect.bottom)
+        }
+    },
+    fill : function(ctx,cam){
+        if (ctx !==undefined){
+            if(cam !== undefined){
+            ctx.fillRect(this.left - cam.x ,this.top - cam.y ,this.width,this.height);
+            }else {
+                ctx.fillRect(this.left,this.top,this.width,this.height);
+            }
+        }
+    },
+    stroke : function(ctx){
+        if(ctx!==undefined){
+            ctx.strokeRect(this.left,this.top,this.width,this.height);   
+        }
+    },
+    drawImageArea: function (ctx,img,sx,sy,sw,sh){
+        if(img.width){
+            ctx.save();
+            ctx.translate(this.x,this.y)
+            ctx.rotate((this.rotation+this.rotationTransition)*Math.PI/180);
+            ctx.drawImage(img,sx,sy,sw,sh,-this.width/2,-this.height/2,this.width,this.height);
+            ctx.restore();
+        }
+    }
+};
+function Camera(){
+    this.x=0;
+    this.y=0;
+}
+Camera.prototype ={
+    constructor : Camera,
+    
+    focus : function(x,y){
+        this.x = x - canvas.width/2
+        this.y = y - canvas.height/2
+        if(this.x<0){
+            this.x=0
+        }else if(this.x+canvas.width>worldWidth){
+            this.x=worldWidth-canvas.width
+        }
+        if(this.y<0){
+            this.y=0
+        }else if(this.y+canvas.height>worldHeight){
+            this.y=worldHeight-canvas.height
+        }
+       
+       
+    }
+};
+function enableInputs (){
+    document.addEventListener('keydown',function(evt){
+        pressing[evt.which]=true;
+        lastPress=evt.which;
+    },false)
+    document.addEventListener('keyup',function(evt){
+        pressing[evt.which]=false;
+    },false)
+}
+
+function init(){
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    canvas.width=300;
+    canvas.height=200;
+
+    player = new Rectangle2D(140,140,10,10,true);
+    cam = new Camera();
+    // wall.push(new Rectangle2D(100,50,10,10,true))
+    // wall.push(new Rectangle2D(100,150,10,10,true))
+    // wall.push(new Rectangle2D(200,50,10,10,true))
+    // wall.push(new Rectangle2D(200,150,10,10,true))
+    enableInputs();
+    setMap(map0,30,20);
+    run()
+}
+function run(){
+    window.requestAnimationFrame(run)
+
+    paint(ctx);
+    act();
+}
+function reset(){
+    player.top=120;
+    player.left=120;
+    gameover=false
+}
+function paint(ctx){
+    ctx.fillStyle='#000';
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle='#fff';
+    player.fill(ctx,cam);
+    ctx.fillStyle='#888';
+    for(i = 0;i<wall.length;i++){
+        wall[i].fill(ctx,cam)
+    }
+    ctx.fillStyle='#f55'
+    for (i=0;i<lava.length;i++){
+        lava[i].fill(ctx,cam);
+    }
+    ctx.font='16px sans-serif';
+    ctx.textAlign='center'
+    ctx.fillStyle='#0f0';
+    if(pause){
+        if(gameover){
+        ctx.fillText('GAMEOVER - ENTER TO RESET',canvas.width/2,canvas.height/2)  
+        }else{
+        ctx.fillText('PAUSE - ENTER TO CONTINUE',canvas.width/2,canvas.height/2)}
+       
+    }
+    ctx.textAlign='left'
+}
+function act (){
+    if(!pause){
+        if(gameover){
+            reset();
+        }
+        cam.focus(player.x,player.y)
+    if(pressing[KEY_LEFT]){
+        player.x-=5;
+        for(var i=0;i<wall.length;i++){
+            if(wall[i].intersects(player)){
+                player.left=wall[i].right
+                }
+            }
+        }else if(pressing[KEY_RIGHT]){
+        player.x+=5;
+        for( i=0;i<wall.length;i++){
+            if(wall[i].intersects(player)){
+                player.right=wall[i].left;
+                }
+            }
+        }else if(pressing[KEY_UP]){
+        player.y-=5;
+        for( i=0;i<wall.length;i++){
+            if(wall[i].intersects(player)){
+                player.top=wall[i].bottom;
+                }
+            }
+        }else if(pressing[KEY_DOWN]){
+        player.y+=5;
+        for( i=0;i<wall.length;i++){
+            if(wall[i].intersects(player)){
+                player.bottom=wall[i].top
+                }
+            }
+        }
+        for(i=0;i<lava.length;i++){
+            if(lava[i].intersects(player)){
+                pause=true;
+                gameover=true;
+            }
+        }
+         if(player.x > worldWidth){
+             player.x=0;
+         }
+         if(player.y > worldHeight){
+             player.y=0;
+         }
+         if(player.x <0){
+             player.x=worldWidth
+         }
+         if(player.y <0){
+             player.y=worldHeight
+         }
+    }
+        if(lastPress===KEY_ENTER){
+            pause=!pause;
+            lastPress=null
+        }
+
+}
+window.addEventListener('load',function(){
+    init()
+},false)
+
+    window.requestAnimationFrame=(function(){
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||function(callback){window.setTimeout(callback,17);};
+    })();
+}(window))
